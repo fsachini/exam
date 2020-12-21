5671c6ce72f8d537ee9399a0e6e8f680fe4e116e
fsachini <75431032+fsachini@users.noreply.github.com>
2020-12-21 12:37:44 +0530

    Update process.php

--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php 
-	$username =$POST['username'];
-	$password =$POST['password'];
+	$username =$_POST['user'];
+	$password =$_POST['pass'];
 
 
 	$username = stripcslashes($username);
@@ -8,10 +8,14 @@
 	$username = mysql_real_escape_string($username);
 	$password = mysql_real_escape_string($password);
 
-	mysql_connect("localhost", "root", "");
-	mysql_select_db("exam");
+	mysqli_connect("localhost", "root", "");
+	mysqli_select_db("exam");
 
 	$result = mysql_query("select *from users where username ='$username' and password ='$password'" ) or die("Failed to query database "mysql_error());
 	$row = mysql_fetch_array($result);
-	if
+	if($row['username']== $username && $row ['password'] == $password) {
+		echo "Login Success!! wecome " . $row['username'];
+	}else{
+		echo"Failed to login.";
+	}
 ?>
